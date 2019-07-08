d8fa936ca7bb0107a9b6a36c669130189b5fbba7
Seth Nadu <seth.nadu@gmail.com>
2019-07-08 18:01:40 -0400

    stretch stretch digital timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -7,20 +7,32 @@ let timerStartmsTens = 0;
 
 
 
+
 //digit variables
 let secondTens = document.getElementById('secondTens');
 let secondOnes = document.getElementById('secondOnes');
 let msHundreds = document.getElementById('msHundreds');
 let msTens = document.getElementById('msTens');
 
+//stretch
+let digits = document.querySelectorAll(".digits");
+let start = document.querySelector(".start");
+let reset = document.querySelector(".reset")
 
 
+function toggle() {
+    start.classList.toggle("hidden");
+    reset.classList.toggle("hidden");
+}
 
-// Tens
+/////////////// timer function ///////////////
+
+function timer () {
+    // Tens
 let timerCountTens = setInterval(() => {
     secondTens.textContent = 1 + timerStartTens;
     timerStartTens +=1;
-    if (timerStartTens >= 1) {
+    if (timerStartTens == 1) {
         clearInterval(timerCountTens);
         document.getElementById('secondTens').style.color = "red";
         document.getElementById('colon').style.color = "red";
@@ -70,3 +82,41 @@ let timerCountmsTens = setInterval(() => {
         document.getElementById('msTens').style.color = "red";
     }
     }, 10);   
+
+
+reset.addEventListener('click', () => {
+    
+    clearInterval(timerCountOnes);
+    clearInterval(timerCountTens);
+    clearInterval(timerCountmsHundreds);
+    clearInterval(timerCountmsTens);
+    let digit = document.querySelectorAll(".digit");
+    let colon = document.querySelector("#colon");
+    digit.forEach((digit) => {
+    digit.textContent = "-";
+    digit.style.color = "black";
+    });
+    colon.textContent = ":";
+    
+    
+});
+}
+
+/////////////////////////  Start Button //////////////
+
+start.addEventListener('click', () => {
+    toggle();
+    timer();
+    timerStartTens = 0;
+    timerStartOnes = 0;
+    timerStartmsHundreds = 0;
+    timerStartmsTens = 0;
+    
+});
+
+reset.addEventListener('click', () => {
+    toggle();
+})
+
+
+
